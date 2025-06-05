2b7cac4ce12a2b066207ba17d27d86374794c43c
DimPap99 <33203177+DimPap99@users.noreply.github.com>
2025-06-05 13:15:23 +0200

    init multiple trees

--- src/main/java/PMIndex/HBI.java
+++ src/main/java/PMIndex/HBI.java
@@ -4,25 +4,49 @@ import algorithms.SearchAlgorithm;
 import membership.BloomFilter;
 import membership.MockMembership;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class HBI implements IPMIndexing {
 
 
     public int windowLength;
+    public int treeLength;
     public int indexedItemsCounter=-1;
-    private ImplicitTree tree;
+
+    double fpRate;
+    int alphabetSize;
+
+    private ArrayList<ImplicitTree> trees;
     public SearchAlgorithm searchAlgo;
-    public HBI(SearchAlgorithm algo, int windowLength, double fpRate, int alphabetSize){
+    public HBI(SearchAlgorithm algo, int windowLength, double fpRate, int alphabetSize, int tree_length){
         this.searchAlgo = algo;
         this.windowLength = windowLength;
-        this.tree = new ImplicitTree(windowLength, new MockMembership(), fpRate, alphabetSize);
+        this.treeLength = tree_length;
+        this.trees = new ArrayList<ImplicitTree>();
+        this.alphabetSize = alphabetSize;
+        this.fpRate = fpRate;
+        this.trees.add(new ImplicitTree(windowLength, new MockMembership(), fpRate, alphabetSize));
+
     }
 
 
+    public void expire(){
+        this.trees.removeFirst();
+    }
     public void insert(String key){
         indexedItemsCounter++;
-        tree.insert(key);
+
+        ImplicitTree lastTree = trees.getLast();
+        if(lastTree.indexedItemsCounter == this.trees.size()){
+            ImplicitTree newLastTree = new ImplicitTree(windowLength, new MockMembership(), fpRate, alphabetSize)
+            newLastTree.insert(key);
+            trees.add(newLastTree);
+        }
+        else{
+            lastTree.insert(key);
+        }
     }
 
     //Simple existence query
