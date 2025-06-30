19e09b31d01ffacc07d306b212f1d8f846f30c67
DimPap99 <33203177+DimPap99@users.noreply.github.com>
2025-06-30 12:37:46 +0200

    latest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,8 @@ public class Main {
     //z00D
     //zs1000
     public static void main(String[] args) throws IOException {
-        String dataFile = "/home/dimpap/IdeaProjects/Hierarchical-Bloom-filter-Index/zipf_text.txt";
-        String queries = "/home/dimpap/IdeaProjects/Hierarchical-Bloom-filter-Index/substrings.txt";
+        String dataFile = "/home/dimpap/Desktop/GraduationProject/Hierarchical-Bloom-filter-Index/Hierarchical-Bloom-filter-Index/zipf_text.txt";
+        String queries = "/home/dimpap/Desktop/GraduationProject/Hierarchical-Bloom-filter-Index/Hierarchical-Bloom-filter-Index/substrings.txt";
         double durationHBI = 0;
         double durationIPM = 0;
         int runs = 50;
@@ -27,27 +27,27 @@ public class Main {
             Supplier<Estimator> factory = () -> new HashMapEstimator(65536);   // same instance
             Supplier<Membership> memFactory = () -> new BloomFilter();
             HBI hbi = new HBI(new BlockSearch(), 131072, 0.001, 81, 65536, factory, memFactory);
-            Experiment.run(dataFile, queries, hbi, false);
-            IPMIndexing index = new RegexIndex();   // switch index implementation
-            Experiment.run(dataFile, queries, index, false);
+            Experiment.run(dataFile, queries, hbi, true);
+//            IPMIndexing index = new RegexIndex();   // switch index implementation
+//            Experiment.run(dataFile, queries, index, false);
         }
 
-        for(int i = 0; i < runs; i++){
-
-            /* build a supplier – could be a lambda or method reference */
-            Supplier<Estimator> factory = () -> new HashMapEstimator(65536);   // same instance
-            Supplier<Membership> memFactory = () -> new BloomFilter();
-
-            HBI hbi = new HBI(new BlockSearch(), 131072, 0.001, 81, 65536, factory, memFactory);
-            durationHBI+= Experiment.run(dataFile, queries, hbi, false);
-            IPMIndexing index = new RegexIndex();   // switch index implementation
-            durationIPM+= Experiment.run(dataFile, queries, index, false);
-        }
-        if(runs > 0){
-            System.out.println("HBI avg (ms):" + (durationHBI/runs));
-            System.out.println("IPM avg (ms):" + (durationIPM/runs));
-
-        }
+//        for(int i = 0; i < runs; i++){
+//
+//            /* build a supplier – could be a lambda or method reference */
+//            Supplier<Estimator> factory = () -> new HashMapEstimator(65536);   // same instance
+//            Supplier<Membership> memFactory = () -> new BloomFilter();
+//
+//            HBI hbi = new HBI(new BlockSearch(), 131072, 0.001, 81, 65536, factory, memFactory);
+//            durationHBI+= Experiment.run(dataFile, queries, hbi, false);
+//            IPMIndexing index = new RegexIndex();   // switch index implementation
+//            durationIPM+= Experiment.run(dataFile, queries, index, false);
+//        }
+//        if(runs > 0){
+//            System.out.println("HBI avg (ms):" + (durationHBI/runs));
+//            System.out.println("IPM avg (ms):" + (durationIPM/runs));
+//
+//        }
 
 
 //        System.out.println("Holding: " + hbi);
